4434b943e0902fac144cfba8dad418934522548d
Md. Wahid Sadiq Ratul <63424209+Ratul-netizen@users.noreply.github.com>
2022-08-01 22:28:34 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -9,16 +9,8 @@
 <link href='https://fonts.googleapis.com/css?family=Gafata|Roboto:700' rel='stylesheet' type='text/css'>
 
 <header class="videohead">
-
-    <!-- Video from coverr.co -->
-    <video class="video-bg" autoplay muted loop>
-        <source src="img/preview.webm" type="video/webm">
-        <source src="img/preview.mp4" type="video/mp4">
-        <source src="img/preview.ogv" type="video/ogg">
-    </video>
     <div class="video-overlay">
         <h1 class="shadow container">Rapid Auto</h1>
-
         <a href="booking.php" class="btn btn-primary mianbutton container" style="text-align: center;">Book Your
             Appoinment !</a>
     </div>
